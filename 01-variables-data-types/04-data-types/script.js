1b6a2411313c581446979c36bad9e00769d7b623
sonlnph38913 <sonlnph38913@fpt.edu.vn>
2024-01-12 20:25:35 +0700

    update file

---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,39 @@
+// String
+const firstName = 'Son';
+
+// Number
+const age = 20;
+const temp = 98.9;
+
+// Boolean
+const hasKids = true;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const n = 9007199254740991n;
+
+// Reference Types
+
+const numbers = [1, 2, 3, 4];
+
+const person = {
+  name: 'Nam',
+};
+
+function sayHello() {
+  console.log('Hello');
+}
+
+const output = sayHello;
+
+console.log(output, typeof output);
+
